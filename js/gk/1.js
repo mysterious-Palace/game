44dbc2c3b2c80d35c8630b696cc72bdc4434affe
mysterious-Palace <60834599+mysterious-Palace@users.noreply.github.com>
2020-04-15 13:00:33 +0800

    Update 1.js

--- js/gk/1.js
+++ js/gk/1.js
@@ -2,22 +2,22 @@
   {
     if(fx=="shang")//上
     {
-      if(map[archive["x"]-1][archive["y"]]==' ')
+      if(map[archive["x"]-1][archive["y"]]!='#')
         archive["x"]--;
     }
     else if(fx=="xia")//下
     {
-      if(map[archive["x"]+1][archive["y"]]==' ')
+      if(map[archive["x"]+1][archive["y"]]!='#')
         archive["x"]++;
     }
     else if(fx=="zuo")//左
     {
-      if(map[archive["x"]][archive["y"]-1]==' ')
+      if(map[archive["x"]][archive["y"]-1]!='#')
         archive["y"]--;
     }
     else if(fx=="you")//右
     {
-      if(map[archive["x"]][archive["y"]+1]==' ')
+      if(map[archive["x"]][archive["y"]+1]!='#')
         archive["y"]++;
     }
     else if(fx=="zhong")
